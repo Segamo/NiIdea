8decbfb406b07c19d680c3e5bee9b6b80473605b
Segamo <sgarcesmo@gmail.com>
2013-05-18 18:58:29 -0500

    First commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Garces
+ * Date: 16/05/13
+ * Time: 12:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HolaMundo {
+
+    main
+
+    public HolaMundo() {
+        super();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
