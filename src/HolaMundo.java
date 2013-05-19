17072c36e3c94323cde81e03f5a72ec45937312c
Segamo <sgarcesmo@gmail.com>
2013-05-18 19:00:02 -0500

    Holiii :3

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,10 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Garces
- * Date: 16/05/13
- * Time: 12:38 AM
- * To change this template use File | Settings | File Templates.
- */
+
 public class HolaMundo {
 
 
